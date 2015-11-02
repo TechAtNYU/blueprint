7b516a18376329112546426f19d62b9cec478e2e
Abhi Agarwal <me@abhiagarwal.com>
2015-11-01 22:11:22 -0500

    Further abstraction

--- app/js/controllers/main-ctrl.js
+++ app/js/controllers/main-ctrl.js
@@ -6,10 +6,40 @@ angular
     $scope.eboard = true;
     $scope.platforms = [
         {
+            "name": "Homepage",
+            "url": "http://techatnyu.org",
+            "description": "Main Tech@NYU site",
+            "for": "Public-facing websites"
+        },
+        {
+            "name": "Cal",
+            "url": "http://cal.techatnyu.org",
+            "description": "Calendar generated using the API",
+            "for": "Public-facing websites"
+        },
+        {
+            "name": "Checkin",
+            "url": "http://checkin.techatnyu.org",
+            "description": "Check people into events",
+            "for": "Public-facing websites"
+        },
+        {
+            "name": "Startup Week",
+            "url": "http://normal.nyusw.com",
+            "description": "Spring 2015 #nyusw site",
+            "for": "Public-facing websites"
+        },
+        {
+            "name": "Career Fair",
+            "url": "http://fair.nyusw.com",
+            "description": "Fall 2015 #nyusw site",
+            "for": "Public-facing websites"
+        },
+        {
             "name": "API",
             "url": "https://api.tnyu.org",
             "description": "Stores/offers access to all Tech@NYU-related data.",
-            "for": "General Internal Platforms"
+            "for": "General Internal Platforms",
         },
         {
             "name": "Intranet",
@@ -46,33 +76,6 @@ angular
             "url": "https://zulip.tnyu.org",
             "description": "Instance of Zulip.",
             "for": "General Internal Platforms"
-        }
-    ];
-    $scope.staticsites = [
-        {
-            "name": "Homepage",
-            "url": "http://techatnyu.org",
-            "description": "Main Tech@NYU site"
-        },
-        {
-            "name": "Cal",
-            "url": "http://cal.techatnyu.org",
-            "description": "Calendar generated using the API"
-        },
-        {
-            "name": "Checkin",
-            "url": "http://checkin.techatnyu.org",
-            "description": "Check people into events"
-        },
-        {
-            "name": "Startup Week",
-            "url": "http://normal.nyusw.com",
-            "description": "Spring 2015 #nyusw site"
-        },
-        {
-            "name": "Career Fair",
-            "url": "http://fair.nyusw.com",
-            "description": "Fall 2015 #nyusw site"
         }
     ];
 });
