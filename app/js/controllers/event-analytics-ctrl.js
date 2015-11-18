4ec6620df1ada70dbc53b8d495698a28cec8e64a
Abhi Agarwal <me@abhiagarwal.com>
2015-11-18 13:24:39 -0500

    Adding primary version of Event feedback

--- app/js/controllers/event-analytics-ctrl.js
+++ app/js/controllers/event-analytics-ctrl.js
@@ -71,9 +71,56 @@ angular
 	};
 
 	var resourceId = $stateParams.id;
-	$scope.loadingPromise = Restangular.one('events/' + resourceId)
+	$scope.loadingPromise = Restangular.one('events/' + resourceId + '?include=survey,feedback')
 		.get()
 		.then(function(data) {
-			$scope.event = data.data;
+			$scope.loadingPromise = Restangular.one('questions')
+				.get()
+				.then(function(questions) {
+					questions = questions.data;
+					var eventData = data.data;
+					var include = data.included;
+					var survey = include[0];
+					var surveyRepsonses = include.slice(1, include.length-1)
+					var questionIdsToQuestions = {};
+
+					_(questions).forEach(function (val) {
+						questionIdsToQuestions[val.id] = val.attributes.text;
+					}).value();
+
+					// Push questions
+					var questions = [];
+					var questionArrayPosition = {};
+					if (survey && survey.relationships && survey.relationships.questions && survey.relationships.questions.data && survey.relationships.questions.data.length > 0) {
+						_(survey.relationships.questions.data).forEach(function (val) {
+							var currentQuestion = {};
+							currentQuestion.name = questionIdsToQuestions[val.id];
+							currentQuestion.answers = [];
+							questions.push(currentQuestion);
+							questionArrayPosition[val.id] = questions.length - 1;
+						}).value();
+					}
+
+					// Find answers
+					if (surveyRepsonses) {
+						_(surveyRepsonses).forEach(function (val) {
+							if (val.relationships && val.relationships.answers && val.relationships.answers.data && val.relationships.answers.data.length > 0) {
+								_(val.relationships.answers.data).forEach(function (singleAnswer, key) {
+									Restangular.one('answers/' + singleAnswer.id)
+										.get()
+										.then(function(answer) {
+											answer = answer.data;
+											var answerQuestionId = answer.relationships && answer.relationships.question && answer.relationships.question.data.id;
+											answerQuestionId = questionArrayPosition[answerQuestionId];
+											questions[answerQuestionId].answers.push({answer: answer.attributes.answer, id: answer.id});
+										});
+								}).value();
+							}
+						}).value();
+					}
+
+					$scope.questions = questions;
+					$scope.event = eventData;
+				});
 		});
 });
